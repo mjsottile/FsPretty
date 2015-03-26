d6f7d92654fe743a5f16d3b09c93226e09d01a9a
Matthew Sottile <mjsottile@gmail.com>
2015-03-25 22:00:21 -0700

    ProjectScaffold starting point

---
+++ src/FsPretty/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FsPretty")>]
+[<assembly: AssemblyProductAttribute("FsPretty")>]
+[<assembly: AssemblyDescriptionAttribute("FSharp implementation of the Wadler prettier printer library.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
