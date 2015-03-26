812fe64efd67a34d26e322d5352c533a9ece1205
Matthew Sottile <mjsottile@gmail.com>
2015-03-25 22:22:19 -0700

    Preliminary release

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,3 @@
+### 0.1.0 - March 25, 2015
+* Preliminary release.
+
--- src/FsPretty/AssemblyInfo.fs
+++ src/FsPretty/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsPretty")>]
 [<assembly: AssemblyProductAttribute("FsPretty")>]
 [<assembly: AssemblyDescriptionAttribute("FSharp implementation of the Wadler prettier printer library.")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "0.1.0"
