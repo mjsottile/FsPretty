9267df3e3e2e8822ec34895562eede8316373675
Matthew Sottile <mjsottile@gmail.com>
2015-03-27 18:21:11 -0700

    a set of examples

--- src/FsPretty/Script.fsx
+++ src/FsPretty/Script.fsx
@@ -1,5 +1,38 @@
 #load "Library.fs"
 open FsPretty.PrettyPrint
 
-let x = semiBraces [text "foo" <**> text "bar"]
-printfn "%A" (displayString x)
+let showit d = d |> displayString |> printfn "%s\n--------"
+
+semiBraces [text "foo" <**> text "bar"] |> showit
+
+(text "Hello") <<>> (mkint 42) |> showit
+
+mkstring "abc\ndef\nghi" |> showit
+
+(text "Hello") <+> (text "world") <+> (mkint 42) |> showit
+
+(text "Hello") <*> (text "world") <*> (mkint 42) |> showit
+
+(text "Hello") <**> (text "world") <**> (mkint 42) |> showit
+
+["this";"is";"a";"test"] |> List.map text |> vcat |> showit
+
+["this";"is";"a";"test"] |> List.map text |> vsep |> showit
+
+["this";"is";"a";"test"] |> List.map text |> hsep |> showit
+
+["this";"is";"a";"test"] |> List.map text |> hcat |> showit
+
+["this";"is";"a";"test"] |> List.map text |> fillSep |> showit
+
+["this";"is";"a";"test"] |> List.map text |> fillCat |> showit
+
+(text "another") </> (text "test") |> showit
+
+(text "another") <//> (text "test") |> showit
+
+(text "string1") <*> nest 1 (text "string2") |> showit
+
+[1..5] |> List.map mkint |> punctuate semi |> hcat |> showit
+
+(text "string1") <*> indent 3 (text "string2") |> showit
